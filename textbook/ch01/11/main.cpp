d1570f522e5e4f28548cadb6db7ae8f682f98622
ChenXinjie04 <xinjiechen04@gmail.com>
2025-03-11 19:42:36 +0800

    ch01 11

--- textbook/ch01/11/main.cpp
+++ textbook/ch01/11/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-	
+	double result = 0;
+	int sign = 1, denominator = 1;
+	for (int i = 0; i < 1000; ++i, sign *= -1, denominator += 2) {
+		result += double(sign) / denominator;
+	}
+	cout << result;
+
 	return 0;
 }
